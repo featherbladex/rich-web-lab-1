b1c551bcba72ac3a55875f671e16a38cb1cc5f6a
featherbladex <57197043+featherbladex@users.noreply.github.com>
2022-10-17 08:43:38 +0100

    Add files via upload

--- problem-2/note_script.js
+++ problem-2/note_script.js
@@ -1,1 +1,26 @@
-var add = document.getElementById("add");
+const noteStorage= document.querySelector("note-display");
+const form= document.querySelector("form");
+
+class addNote{
+    constructor(title,body){
+        this.title=title;
+        this.body=body;
+
+    }
+    
+}
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const titleInput = document.querySelector("#title")
+    const noteInput = document.querySelector("#note")
+
+    if(titleInput.value.length>0 && noteInput.value.length>0){
+        const newNote = new addNote(titleInput,noteInput);
+        addNotetoList(newNote);
+        titleInput.value="";
+        noteInput.value="";
+        titleInput.focus();
+
+    }
+
+})
